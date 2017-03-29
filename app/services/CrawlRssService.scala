8b0ba37b257d8f1891d80042239982531f35970b
tmknom <tmk.nom@gmail.com>
2017-03-29 13:03:17 +0900

    CrawlRssServiceがList[ArticleEntity]を返すよう変更

--- app/services/CrawlRssService.scala
+++ app/services/CrawlRssService.scala
@@ -2,19 +2,20 @@ package services
 
 import javax.inject.{Inject, Singleton}
 
+import domains.article.ArticleEntity
 import domains.crawler.HatenaBookmarkAdapter
 
 trait CrawlRssService {
-  def perform(): Map[String, String]
+  def perform(): List[ArticleEntity]
 }
 
 @Singleton
 class CrawlRssServiceImpl @Inject() (hatenaBookmarkAdapter: HatenaBookmarkAdapter) extends CrawlRssService {
-  override def perform(): Map[String, String] = {
+  override def perform(): List[ArticleEntity] = {
     val articleEntities = hatenaBookmarkAdapter.crawl()
     for (articleEntity <- articleEntities){
       println(articleEntity.title + " : " + articleEntity.url)
     }
-    Map("status" -> "OK", "method" -> "post")
+    articleEntities
   }
 }
