98a2bd83f3257315ab6a98ae9dc5c4258c0c7037
tmknom <tmk.nom@gmail.com>
2017-04-01 21:58:47 +0900

    Fixtureをロードするユーティリティクラス作成

---
+++ test/base/scalatest/utility/FixtureLoader.scala
@@ -0,0 +1,28 @@
+package base.scalatest.utility
+
+import scala.io.Source
+
+/**
+  * Fixtureをロードするユーティリティクラス
+  *
+  * test/fixtures ディレクトリ配下のファイルをロード
+  */
+object FixtureLoader {
+  /**
+    * Fixture をロードする
+    *
+    * 例えば test/fixtures/hoge/fuga.txt をロードする場合
+    * val fixture = FixtureLoader.load("/hoge/fuga")
+    *
+    * @param path フィクスチャのパス
+    * @return フィクスチャ
+    */
+  def load(path: String): String = {
+    val source = Source.fromInputStream(getClass.getResourceAsStream(path))
+    try {
+      source.mkString
+    } finally {
+      source.close()
+    }
+  }
+}
