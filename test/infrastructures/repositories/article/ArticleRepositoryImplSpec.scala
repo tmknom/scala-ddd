98f0752b6d5bda6de489a22639c0c59829473fc2
tmknom <tmk.nom@gmail.com>
2017-04-01 13:55:50 +0900

    ArticleRepositoryImplSpecに参照系のデータベーステスト作成

--- test/infrastructures/repositories/article/ArticleRepositoryImplSpec.scala
+++ test/infrastructures/repositories/article/ArticleRepositoryImplSpec.scala
@@ -16,4 +16,32 @@ class ArticleRepositoryImplSpec extends DatabaseSpec with ScalaFutures {
       }
     }
   }
+
+  "listAll" should {
+    "success" in {
+      val articleEntity = ArticleEntity(None, "sample_title", "sample_url")
+      whenReady(articleRepository.insert(articleEntity)) { _ =>
+        whenReady(articleRepository.listAll()) { actual =>
+          // 件数チェック
+          actual must have size 1
+
+          // Entityの属性をチェック
+          actual.head.id must not be null
+          actual.head.title mustBe articleEntity.title
+          actual.head.url mustBe articleEntity.url
+
+          // Entityの属性をチェック：プロパティチェックの構文で書いた場合
+          // こちらのほうが見やすい気はするが、属性名を 'title みたいな変な構文で書かないといけない
+          // http://www.scalatest.org/user_guide/using_matchers#checkingArbitraryProperties
+          //
+          // この書き方だと、型が分からないので、IntelliJが助けてくれない
+          // 属性名をリファクタリングする時に、追従してくれないため、使わないほうが良さそう
+          actual.head must have (
+            'title (articleEntity.title),
+            'url (articleEntity.url)
+          )
+        }
+      }
+    }
+  }
 }
