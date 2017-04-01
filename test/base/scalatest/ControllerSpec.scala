39adae7bff07dfde8cd5ef6ad01d9915955edad3
tmknom <tmk.nom@gmail.com>
2017-04-01 16:33:59 +0900

    ControllerSpecからOneAppPerTestのmixinを削除

--- test/base/scalatest/ControllerSpec.scala
+++ test/base/scalatest/ControllerSpec.scala
@@ -1,8 +1,8 @@
 package base.scalatest
 
 import org.scalatest.mock.MockitoSugar
-import org.scalatestplus.play.{OneAppPerTest, PlaySpec}
+import org.scalatestplus.play.PlaySpec
 
-trait ControllerSpec extends PlaySpec with OneAppPerTest with MockitoSugar {
+trait ControllerSpec extends PlaySpec with MockitoSugar {
 
 }
