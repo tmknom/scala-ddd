a80fb126164685336e9e8d0b1d73303b399aca36
tmknom <tmk.nom@gmail.com>
2017-04-29 15:52:04 +0900

    Filtersの「Public member must have an explicit type ascription」を駆逐

--- app/Filters.scala
+++ app/Filters.scala
@@ -23,7 +23,7 @@ class Filters @Inject() (
   env: Environment,
   exampleFilter: ExampleFilter) extends HttpFilters {
 
-  override val filters = {
+  override val filters: Seq[Filter] = {
     // Use the example filter if we're running development mode. If
     // we're running in production or test mode then don't use any
     // filters at all.
