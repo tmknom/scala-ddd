054e068a42ea4b29e733b0244dfe7a579deff428
tmknom <tmk.nom@gmail.com>
2017-04-29 23:01:22 +0900

    ArticleRepositoryをskinnyで再実装

--- app/infrastructures/article/ArticleRepositoryImpl.scala
+++ app/infrastructures/article/ArticleRepositoryImpl.scala
@@ -1,33 +1,24 @@
 package infrastructures.article
 
-import javax.inject.Inject
-
 import domains.article.{ArticleEntity, ArticleRepository}
-import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import slick.driver.JdbcProfile
+import scalikejdbc._
+import skinny.orm.{Alias, SkinnyCRUDMapper}
 
-import scala.concurrent.Future
+class ArticleRepositoryImpl extends SkinnyCRUDMapper[ArticleEntity] with ArticleRepository {
+  override lazy val defaultAlias: Alias[ArticleEntity] = createAlias("a")
+  override lazy val tableName: String = "articles"
+  private[this] lazy val a = defaultAlias
 
-class ArticleRepositoryImpl @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
-  extends HasDatabaseConfigProvider[JdbcProfile] with ArticleRepository {
+  override def extract(rs: WrappedResultSet, rn: ResultName[ArticleEntity]): ArticleEntity = ArticleEntity(
+    id = Some(rs.int(rn.id)),
+    title = rs.string(rn.title),
+    url = rs.string(rn.url)
+  )
 
-  import driver.api._
+  override def listAll(): Seq[ArticleEntity] = findAll()
 
-  private val Articles = TableQuery[ArticlesTable]
-
-  override def listAll(): Future[Seq[ArticleEntity]] = db.run(Articles.result)
-
-  override def insert(articleEntity: ArticleEntity): Future[Unit] = db.run(Articles += articleEntity).map { _ => () }
-
-  // scalastyle:off
-  @SuppressWarnings(Array("org.wartremover.warts.Nothing"))
-  private class ArticlesTable(tag: Tag) extends Table[ArticleEntity](tag, "articles") {
-    def id = column[Int]("id", O.PrimaryKey)
-    def title = column[String]("title")
-    def url = column[String]("url")
-
-    def * = (id.?, title, url) <> (ArticleEntity.tupled, ArticleEntity.unapply _)
-  }
-  // scalastyle:on
+  override def insert(articleEntity: ArticleEntity): Long = createWithNamedValues(
+    column.title -> articleEntity.title,
+    column.url -> articleEntity.url
+  )
 }
--- test/infrastructures/article/ArticleRepositoryImplSpec.scala
+++ test/infrastructures/article/ArticleRepositoryImplSpec.scala
@@ -2,47 +2,45 @@ package infrastructures.article
 
 import cores.test.scalatest.DatabaseSpec
 import domains.article.{ArticleEntity, ArticleRepository}
-import org.scalatest.concurrent.ScalaFutures
 import play.api.Application
 
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter", "org.wartremover.warts.Null", "org.wartremover.warts.TraversableOps"))
-class ArticleRepositoryImplSpec extends DatabaseSpec with ScalaFutures {
+class ArticleRepositoryImplSpec extends DatabaseSpec {
   def articleRepository(implicit app: Application): ArticleRepository = Application.instanceCache[ArticleRepositoryImpl].apply(app)
 
   "insert" should {
     "success" in {
       val articleEntity = ArticleEntity(None, "sample_title", "sample_url")
-      whenReady(articleRepository.insert(articleEntity)) { actual =>
-        actual mustBe((): Unit)
-      }
+      val actual = articleRepository.insert(articleEntity)
+      // 絶対もっとマシな書き方あるだろ。。
+      actual > 0 mustBe true
     }
   }
 
   "listAll" should {
     "success" in {
       val articleEntity = ArticleEntity(None, "sample_title", "sample_url")
-      whenReady(articleRepository.insert(articleEntity)) { _ =>
-        whenReady(articleRepository.listAll()) { actual =>
-          // 件数チェック
-          actual must have size 1
+      articleRepository.insert(articleEntity)
+      val actual = articleRepository.listAll()
 
-          // Entityの属性をチェック
-          actual.head.id must not be null // scalastyle:ignore
-          actual.head.title mustBe articleEntity.title
-          actual.head.url mustBe articleEntity.url
+      // 件数チェック
+      actual must have size 1
 
-          // Entityの属性をチェック：プロパティチェックの構文で書いた場合
-          // こちらのほうが見やすい気はするが、属性名を 'title みたいな変な構文で書かないといけない
-          // http://www.scalatest.org/user_guide/using_matchers#checkingArbitraryProperties
-          //
-          // この書き方だと、型が分からないので、IntelliJが助けてくれない
-          // 属性名をリファクタリングする時に、追従してくれないため、使わないほうが良さそう
-          actual.head must have (
-            'title (articleEntity.title),
-            'url (articleEntity.url)
-          )
-        }
-      }
+      // Entityの属性をチェック
+      actual.head.id must not be null // scalastyle:ignore
+      actual.head.title mustBe articleEntity.title
+      actual.head.url mustBe articleEntity.url
+
+      // Entityの属性をチェック：プロパティチェックの構文で書いた場合
+      // こちらのほうが見やすい気はするが、属性名を 'title みたいな変な構文で書かないといけない
+      // http://www.scalatest.org/user_guide/using_matchers#checkingArbitraryProperties
+      //
+      // この書き方だと、型が分からないので、IntelliJが助けてくれない
+      // 属性名をリファクタリングする時に、追従してくれないため、使わないほうが良さそう
+      actual.head must have(
+        'title (articleEntity.title),
+        'url (articleEntity.url)
+      )
     }
   }
 }
