39b74d3b10e0ca5aca3f98a02200413b61d4ac1d
tmknom <tmk.nom@gmail.com>
2017-04-29 16:25:07 +0900

    ArticleRepositoryImplの「Inferred type containing Nothing」を抑制

--- app/infrastructures/article/ArticleRepositoryImpl.scala
+++ app/infrastructures/article/ArticleRepositoryImpl.scala
@@ -21,6 +21,7 @@ class ArticleRepositoryImpl @Inject()(protected val dbConfigProvider: DatabaseCo
   override def insert(articleEntity: ArticleEntity): Future[Unit] = db.run(Articles += articleEntity).map { _ => () }
 
   // scalastyle:off
+  @SuppressWarnings(Array("org.wartremover.warts.Nothing"))
   private class ArticlesTable(tag: Tag) extends Table[ArticleEntity](tag, "articles") {
     def id = column[Int]("id", O.PrimaryKey)
     def title = column[String]("title")
