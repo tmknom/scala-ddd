8f3b532f6d5a27ff75bb10491db95111ff287b33
tmknom <tmk.nom@gmail.com>
2017-04-29 16:17:48 +0900

    ArticleEntityの「Function has default arguments」を抑制

--- app/domains/article/ArticleEntity.scala
+++ app/domains/article/ArticleEntity.scala
@@ -1,5 +1,6 @@
 package domains.article
 
+@SuppressWarnings(Array("org.wartremover.warts.DefaultArguments"))
 case class ArticleEntity(
                           id: Option[Int] = None,
                           title: String,
