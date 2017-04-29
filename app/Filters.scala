f6d441214dc28816c4eae03508a222095cbfca64
tmknom <tmk.nom@gmail.com>
2017-04-29 18:09:29 +0900

    オブジェクトの比較を===メソッドに差し替え

--- app/Filters.scala
+++ app/Filters.scala
@@ -1,9 +1,10 @@
 import javax.inject._
+
+import cores.equality.TripleEquals._
+import filters.ExampleFilter
 import play.api._
 import play.api.http.HttpFilters
 import play.api.mvc._
-
-import filters.ExampleFilter
 
 /**
  * This class configures filters that run on every request. This
@@ -27,7 +28,7 @@ class Filters @Inject() (
     // Use the example filter if we're running development mode. If
     // we're running in production or test mode then don't use any
     // filters at all.
-    if (env.mode == Mode.Dev) Seq(exampleFilter) else Seq.empty[Filter]
+    if (env.mode === Mode.Dev) Seq(exampleFilter) else Seq.empty[Filter]
   }
 
 }
