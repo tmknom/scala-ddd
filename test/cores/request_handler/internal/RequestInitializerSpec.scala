46006bc46bf8aecaf2d8ca25e8c78d01dc14caeb
tmknom <tmk.nom@gmail.com>
2017-05-01 16:52:06 +0900

    RequestInitializerSpecのテスト作成

---
+++ test/cores/request_handler/internal/RequestInitializerSpec.scala
@@ -0,0 +1,13 @@
+package cores.request_handler.internal
+
+import org.scalatestplus.play.PlaySpec
+import play.api.test.FakeRequest
+
+class RequestInitializerSpec extends PlaySpec {
+  "RequestInitializer#initialize" should {
+    "リクエストヘッダーにタグがセットされていること" in {
+      val actual = RequestInitializer.initialize(FakeRequest())
+      actual.tags(RequestHeaderTagName.RequestId).length mustBe 36
+    }
+  }
+}
