91932bf013e2820aae99327672574f6e519c8ff3
tmknom <tmk.nom@gmail.com>
2017-05-01 18:14:14 +0900

    MDCの値をスレッド間で引き回すMDCHttpExecutionContextを作成

---
+++ app/cores/execution_context/MDCHttpExecutionContext.scala
@@ -0,0 +1,54 @@
+package cores.execution_context
+
+import java.util.Map
+
+import org.slf4j.MDC
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+
+/**
+  * slf4j provides a MDC [[http://logback.qos.ch/manual/mdc.html Mapped Diagnostic Context]]
+  * based on a [[ThreadLocal]]. In an asynchronous environment, the callbacks can be called
+  * in another thread, where the local thread variable does not exist anymore.
+  *
+  * This execution context fixes this problem:
+  * it propagates the MDC from the caller's thread to the callee's one.
+  *
+  * @see http://qiita.com/castersupermild/items/b5682828307430e2cc9a
+  * @see http://yanns.github.io/blog/2014/05/04/slf4j-mapped-diagnostic-context-mdc-with-play-framework/
+  */
+private[execution_context] object MDCHttpExecutionContext {
+
+  /**
+    * Create an MDCHttpExecutionContext with values from the current thread.
+    */
+  def fromThread(delegate: ExecutionContext): ExecutionContextExecutor =
+    new MDCHttpExecutionContext(MDC.getCopyOfContextMap, delegate)
+}
+
+/**
+  * Manages execution to ensure that the given MDC context are set correctly
+  * in the current thread. Actual execution is performed by a delegate ExecutionContext.
+  */
+private[execution_context] final class MDCHttpExecutionContext(mdcContext: Map[String, String], delegate: ExecutionContext) extends ExecutionContextExecutor {
+  def execute(runnable: Runnable): Unit = delegate.execute(new Runnable {
+    def run() {
+      val oldMDCContext = MDC.getCopyOfContextMap
+      setContextMap(mdcContext)
+      try {
+        runnable.run()
+      } finally {
+        setContextMap(oldMDCContext)
+      }
+    }
+  })
+
+  private[this] def setContextMap(context: Map[String, String]) {
+    Option(context) match {
+      case Some(c) => MDC.setContextMap(c)
+      case None => MDC.clear()
+    }
+  }
+
+  def reportFailure(t: Throwable): Unit = delegate.reportFailure(t)
+}
