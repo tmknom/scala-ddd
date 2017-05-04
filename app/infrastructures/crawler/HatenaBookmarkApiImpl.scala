9f61ccfb64942d4b76a4623a473b66f31abdb1cc
tmknom <tmk.nom@gmail.com>
2017-05-04 12:58:04 +0900

    HatenaBookmarkApiImplの定数名をリファクタ

--- app/infrastructures/crawler/HatenaBookmarkApiImpl.scala
+++ app/infrastructures/crawler/HatenaBookmarkApiImpl.scala
@@ -9,12 +9,12 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class HatenaBookmarkApiImpl extends HatenaBookmarkApi {
-  private val RSS_URL = "http://b.hatena.ne.jp/entrylist/it?sort=hot&threshold=30&mode=rss"
-  private val FAKE_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5)"
+  private val RssUrl = "http://b.hatena.ne.jp/entrylist/it?sort=hot&threshold=30&mode=rss"
+  private val FakeUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5)"
 
   override def request(): Future[String] = {
     // はてなブックマークはユーザエージェントでアクセスが弾かれるケースがあるので、ダミーをセットしている
-    val svc = url(RSS_URL).setHeader("User-Agent", FAKE_USER_AGENT)
+    val svc = url(RssUrl).setHeader("User-Agent", FakeUserAgent)
     Http(svc OK as.String)
   }
 }
