e1961ab31899e0c9319ad53c53f7302c577dbe34
tmknom <tmk.nom@gmail.com>
2017-05-05 17:53:20 +0900

    ApplicationTimerの削除

--- app/Module.scala
+++ app/Module.scala
@@ -7,7 +7,6 @@ import infrastructures.article._
 import infrastructures.crawler._
 import services.article._
 import services.crawler._
-import services.{ApplicationTimer}
 
 /**
  * This class is a Guice module that tells Guice how to bind several
@@ -38,9 +37,6 @@ class Module extends AbstractModule {
   override def configure(): Unit = {
     // Use the system clock as the default implementation of Clock
     bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
-    // Ask Guice to create an instance of ApplicationTimer when the
-    // application starts.
-    bind(classOf[ApplicationTimer]).asEagerSingleton()
 
     // とりあえずレイヤ単位でメソッドを切っておく
     configureServices()
