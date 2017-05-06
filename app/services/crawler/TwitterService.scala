36a83b4397e24ef78f8a816495e5f02a7cc5f871
tmknom <tmk.nom@gmail.com>
2017-05-06 16:51:17 +0900

    TwitterServiceにDB保存処理を追加

--- app/services/crawler/TwitterService.scala
+++ app/services/crawler/TwitterService.scala
@@ -3,7 +3,7 @@ package services.crawler
 import javax.inject.{Inject, Singleton}
 
 import domains.crawler.TwitterApi
-import domains.tweet.TweetEntity
+import domains.tweet.{TweetEntity, TweetRepository}
 
 trait TwitterService {
   def crawl(): Seq[TweetEntity]
@@ -11,10 +11,12 @@ trait TwitterService {
 
 @Singleton
 final class TwitterServiceImpl @Inject()(
-                                          api: TwitterApi) extends TwitterService {
+                                          api: TwitterApi,
+                                          tweetRepository: TweetRepository) extends TwitterService {
 
   override def crawl(): Seq[TweetEntity] = {
     val response = api.request()
+    response.foreach(tweetRepository.insert)
     response
   }
 }
