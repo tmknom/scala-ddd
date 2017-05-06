af43615edc4228efb6e5d458c090e42a3efeb042
tmknom <tmk.nom@gmail.com>
2017-05-06 16:49:31 +0900

    TweetRepositoryを実装

---
+++ app/infrastructures/tweet/TweetRepositoryImpl.scala
@@ -0,0 +1,36 @@
+package infrastructures.tweet
+
+import java.time.ZonedDateTime
+
+import domains.tweet.{TweetEntity, TweetRepository}
+import scalikejdbc._
+import scalikejdbc.jsr310._
+import skinny.orm.{Alias, SkinnyCRUDMapper}
+
+class TweetRepositoryImpl extends SkinnyCRUDMapper[TweetEntity] with TweetRepository {
+  override lazy val defaultAlias: Alias[TweetEntity] = createAlias("a")
+  override lazy val tableName: String = "tweets"
+
+  override def extract(rs: WrappedResultSet, rn: ResultName[TweetEntity]): TweetEntity = TweetEntity(
+    id = Some(rs.int(rn.id)),
+    originId = rs.long(rn.originId),
+    userScreenName = rs.string(rn.userScreenName),
+    text = rs.string(rn.text),
+    retweetCount = rs.int(rn.retweetCount),
+    favoriteCount = rs.int(rn.favoriteCount),
+    lang = rs.string(rn.lang),
+    createdDateTime = rs.get[ZonedDateTime](rn.createdDateTime)
+  )
+
+  override def listAll(): Seq[TweetEntity] = findAll()
+
+  override def insert(tweetEntity: TweetEntity): Long = createWithNamedValues(
+    column.originId -> tweetEntity.originId,
+    column.userScreenName -> tweetEntity.userScreenName,
+    column.text -> tweetEntity.text,
+    column.retweetCount -> tweetEntity.retweetCount,
+    column.favoriteCount -> tweetEntity.favoriteCount,
+    column.lang -> tweetEntity.lang,
+    column.createdDateTime -> tweetEntity.createdDateTime
+  )
+}
