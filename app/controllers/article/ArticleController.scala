f48e1b73c96acaaa62aaa9108ed793466bdc8f1b
tmknom <tmk.nom@gmail.com>
2017-05-17 14:21:14 +0900

    検索クエリのバリデーション追加

--- app/controllers/article/ArticleController.scala
+++ app/controllers/article/ArticleController.scala
@@ -3,8 +3,12 @@ package controllers.article
 import javax.inject._
 
 import cores.traceable.TraceableAction
+import cores.validation.FormValidation
 import domains.article.ArticleEntity
 import domains.article.ArticleEntityJsonProtocol._
+import domains.search.SearchQuery
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
 import services.article.ArticleService
 import spray.json._
@@ -17,8 +21,15 @@ class ArticleController @Inject()(articleService: ArticleService) extends Contro
     Ok(articleEntities.toJson.prettyPrint)
   }
 
-  def search(query: String): Action[AnyContent] = TraceableAction {
-    val articleEntities: Seq[ArticleEntity] = articleService.search(query)
+  def search(query: String): Action[AnyContent] = TraceableAction { implicit request =>
+    val searchQuery = FormValidation.validate(searchForm)
+    val articleEntities: Seq[ArticleEntity] = articleService.search(searchQuery.value)
     Ok(articleEntities.toJson.prettyPrint)
   }
+
+  val searchForm: Form[SearchQuery] = Form(
+    mapping(
+      "query" -> nonEmptyText
+    )(SearchQuery.apply)(SearchQuery.unapply)
+  )
 }
