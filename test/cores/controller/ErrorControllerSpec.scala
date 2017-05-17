90dbbea579cb8695cd126227c2d3f800dfa94716
tmknom <tmk.nom@gmail.com>
2017-05-17 10:27:43 +0900

    ErrorControllerのテストケースをリファクタリング

--- test/cores/controller/ErrorControllerSpec.scala
+++ test/cores/controller/ErrorControllerSpec.scala
@@ -1,12 +1,29 @@
 package cores.controller
 
 import org.scalatestplus.play._
+import play.api.libs.json.Json
 
 class ErrorControllerSpec extends PlaySpec with OneServerPerTest with OneBrowserPerTest with HtmlUnitFactory {
   "ErrorController#index" when {
     "success" in {
       go to ("http://localhost:" + port.toString + "/error")
-      pageSource must include ("This is error!")
+
+      val actual = Json.parse(pageSource)
+      val requestId = (actual \ "request_id").as[String]
+
+      // リクエストIDは毎回変わるので長さだけ確認
+      requestId.length mustBe 36
+
+      // リクエストIDは動的に埋め込まないといけない
+      actual mustBe Json.parse(
+        s"""{
+           |  "errors": [{
+           |    "code": "anon$$1",
+           |    "message": "Execution exception[[RuntimeException: This is error!]]"
+           |  }],
+           |  "status_code": 500,
+           |  "request_id": "$requestId"
+           |}""".stripMargin)
     }
   }
 }
